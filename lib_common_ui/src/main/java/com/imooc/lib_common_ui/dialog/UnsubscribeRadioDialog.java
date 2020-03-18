492ecdfd37d6dcb556338c9dd4862ba5f59e94d5
zhangruiping <1025618933@qq.com>
2020-03-18 09:53:33 +0800

    添加取消订阅电台Dialog

---
+++ lib_common_ui/src/main/java/com/imooc/lib_common_ui/dialog/UnsubscribeRadioDialog.java
@@ -0,0 +1,44 @@
+package com.imooc.lib_common_ui.dialog;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+import com.imooc.lib_common_ui.R;
+import com.lxj.xpopup.core.CenterPopupView;
+
+public class UnsubscribeRadioDialog extends CenterPopupView {
+
+    IConfirmUnsubscribeListener listener;
+
+    public UnsubscribeRadioDialog(@NonNull Context context, IConfirmUnsubscribeListener listener) {
+        super(context);
+        this.listener = listener;
+    }
+
+    @Override
+    protected int getImplLayoutId() {
+        return R.layout.dialog_radio_unsubscribe;
+    }
+
+    @Override
+    protected void onCreate() {
+        super.onCreate();
+        findViewById(R.id.tv_radio_sub_cancel).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss(); // 关闭弹窗
+            }
+        });
+        findViewById(R.id.tv_radio_sub_confirm).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onConfirm();
+            }
+        });
+    }
+
+    interface IConfirmUnsubscribeListener{
+        void onConfirm();
+    }
+}
