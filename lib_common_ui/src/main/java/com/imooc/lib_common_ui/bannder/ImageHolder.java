18572ded8c136d91013858f01de1d05df92cbf92
zion223 <1025618933@qq.com>
2020-11-24 13:43:05 +0800

    refactor: 删除不需要的代码

--- lib_common_ui/src/main/java/com/imooc/lib_common_ui/bannder/ImageHolder.java
+++ lib_common_ui/src/main/java/com/imooc/lib_common_ui/bannder/ImageHolder.java
@@ -5,20 +5,11 @@ import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
 
 import com.bigkoo.convenientbanner.holder.Holder;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
 import com.imooc.lib_image_loader.app.ImageLoaderManager;
 
 public class ImageHolder implements Holder<String> {
 
     private AppCompatImageView mImageView;
-
-    private static final RequestOptions BANNER_OPTIONS = new RequestOptions()
-            .centerCrop()
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .dontAnimate();
-
-
 
     @Override
     public View createView(Context context) {
@@ -28,7 +19,7 @@ public class ImageHolder implements Holder<String> {
 
     @Override
     public void UpdateUI(Context context, int position, String data) {
-
+        // 更新UI 使用图片加载库加载图片文件
         ImageLoaderManager.getInstance().displayImageForCorner(mImageView, data);
     }
 }
